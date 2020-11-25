5aed528d7844209119edd9f51cb0d353f1ce47d6
nkrokhmal <krokhmal11@mail.ru>
2020-11-25 21:10:23 +0300

    Fix

--- app/utils/openpyxl_wrapper.py
+++ app/utils/openpyxl_wrapper.py
@@ -16,17 +16,22 @@ class ExcelBlock:
         if set_font:
             self.sheet.cell(row, col).font = Font(size=self.font_size)
 
-    def cell_value(self, row, col, value, set_colour=True, set_font=True):
+    def cell_value(self, row, col, value, alignment=None, set_colour=True, set_font=True):
         self.sheet.cell(row, col).value = value
         self.default_colour(row, col, set_colour)
         self.default_font(row, col, set_font)
+        if alignment is not None:
+            self.sheet.cell(row, col).alignment = alignment
 
-    def merge_cells(self, beg_row, end_row, beg_col, end_col, value, set_colour=True, set_font=True):
+    def merge_cells(self, beg_row, end_row, beg_col, end_col, value, alignment=None, set_colour=True, set_font=True):
         self.sheet.merge_cells(start_row=beg_row,
                                end_row=end_row,
-                               start_col=beg_col,
-                               end_col=end_col)
-        self.cell_value(row=beg_row, col=beg_col, value=value, set_colour=set_colour, set_font=set_font)
+                               start_column=beg_col,
+                               end_column=end_col)
+        self.cell_value(row=beg_row, col=beg_col, value=value, alignment=alignment, set_colour=set_colour, set_font=set_font)
+        self.default_colour(beg_row, beg_col, set_colour)
+        self.default_font(beg_row, beg_col, set_font)
+
 
 
 
