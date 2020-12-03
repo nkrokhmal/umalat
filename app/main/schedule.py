a4e23f0c8411e7690eb575d91db0a1cbe9953bb4
nkrokhmal <krokhmal11@mail.ru>
2020-12-03 15:17:52 +0300

    Fix

--- app/main/schedule.py
+++ app/main/schedule.py
@@ -11,6 +11,8 @@ from pycel import ExcelCompiler
 from utils_ak.interactive_imports import *
 from app.schedule_maker.algo import *
 from config import basedir
+import datetime
+
 
 
 @main.route('/schedule', methods=['GET', 'POST'])
@@ -29,18 +31,29 @@ def schedule():
         wb.save(path)
 
         filename_schedule = '{}_{}.xlsx'.format('schedule', date.strftime('%Y-%m-%d'))
+        json_schedule = '{}_{}.txt'.format('schedule', date.strftime('%Y-%m-%d'))
+
         path_schedule = '{}/{}'.format('app/data/schedule', filename_schedule)
+        path_json_schedule = '{}/{}'.format('app/data/schedule_json', json_schedule)
 
         excel = ExcelCompiler(path)
 
         wb.create_sheet('планирование по цехам')
 
         boiling_request = parse_plan_cell(date=date, wb=wb, excel=excel, skus=skus)
+        with open(path_json_schedule, 'w') as outfile:
+            def default(o):
+                if isinstance(o, (datetime.date, datetime.datetime)):
+                    return o.isoformat()
+            json.dump(boiling_request, outfile, default=default)
+
         root = make_schedule(boiling_request, date)
         schedule_wb = draw_workbook(root, mode='prod')
         schedule_wb.save(path_schedule)
 
         schedule_link = '{}/{}'.format('data/schedule', filename_schedule)
-        return render_template('schedule.html', form=form, schedule_link=schedule_link)
+        schedule_json_link = '{}/{}'.format('data/schedule_json', json_schedule)
+        return render_template('schedule.html', form=form, schedule_link=schedule_link, schedule_json_link=schedule_json_link)
     schedule_link = None
-    return render_template('schedule.html', form=form, schedule_link=schedule_link)
+    schedule_json_link = None
+    return render_template('schedule.html', form=form, schedule_link=schedule_link, schedule_json_link=schedule_json_link)
