ec15d6096a2bbb8577ccd1325bf97561829aae5b
nkrokhmal <krokhmal11@mail.ru>
2021-01-05 17:35:02 +0300

    Fix

--- app/utils/generate_constructor.py
+++ app/utils/generate_constructor.py
@@ -286,8 +286,12 @@ def draw_constructor_template(df, file_name, wb, batch_number=0):
                     colour = get_colour_by_name(v[5], skus)
                 else:
                     colour = current_app.config['COLOURS']['Remainings']
-                v[0] = '=IF(J{0}="-", "", 1 + {1} + SUM(INDIRECT(ADDRESS(2,COLUMN(M{0})) & ":" & ADDRESS(ROW(),COLUMN(M{0})))))'.format(
-                    cur_i, batch_number)
+                if sheet_name == 'Вода':
+                    v[0] = '=IF(J{0}="-", "", 1 + {1} + SUM(INDIRECT(ADDRESS(2,COLUMN(M{0})) & ":" & ADDRESS(ROW(),COLUMN(M{0})))))'.format(
+                        cur_i, batch_number)
+                else:
+                    v[0] = '=IF(J{0}="-", "", 1 + MAX(\'Вода\'!A2:A100) + {1} + SUM(INDIRECT(ADDRESS(2,COLUMN(M{0})) & ":" & ADDRESS(ROW(),COLUMN(M{0})))))'.format(
+                        cur_i, batch_number)
                 v[1] = '=IF(J{0}="-", "", 1 + SUM(INDIRECT(ADDRESS(2,COLUMN(M{0})) & ":" & ADDRESS(ROW(),COLUMN(M{0})))))'.format(
                     cur_i)
                 draw_row(boiling_sheet, cur_i, v[:-1], font_size=8, color=colour)
