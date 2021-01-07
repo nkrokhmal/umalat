152d998a4a0f379708522b817b744db9a4122990
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-01-07 15:26:55 +0300

    fixed packing reconfiguration to Ulma only

--- app/schedule_maker/blocks/packing.py
+++ app/schedule_maker/blocks/packing.py
@@ -1,10 +1,12 @@
 from app.schedule_maker.models import *
 
 
-# todo: take from parameters
 def get_configuration_time(boiling_model, sku1, sku2):
-    # todo: make properly
-    if boiling_model.boiling_type == 'salt' and sku1.weight_form_factor != sku2.weight_form_factor and cast_packer(sku1) == cast_packer(sku2):
+    # todo: test
+    if all([boiling_model.boiling_type == 'salt',
+            sku1.weight_form_factor != sku2.weight_form_factor,
+            cast_packer(sku1) == cast_packer(sku2),
+            cast_packer(sku1).name == 'Ульма']):
         return 25
     elif sku1 == sku2:
         return 0
