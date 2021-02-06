eadd9de941744369fa31f17dd903336c60693dae
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-02-06 13:52:22 +0300

    optimization: make full_cleaning after water boilings

--- app/schedule_maker/algo/schedule/schedule_optimization.py
+++ app/schedule_maker/algo/schedule/schedule_optimization.py
@@ -8,61 +8,35 @@ def make_schedule_with_boiling_inside_a_day(boiling_plan_df, start_times=None, f
     start_times = start_times or {LineName.WATER: '08:00', LineName.SALT: '07:00'}
     res = {}
 
+    # make first boiling - no cleaning boilings
     boilings = make_boilings(boiling_plan_df)
-    # cleaning_boilings = [None] + boilings[0: len(boilings) - 1]
+    schedule = make_schedule(boilings, start_times=start_times)
+    res[None] = calc_schedule_stats(schedule)
 
-    water_boilings = [boiling for boiling in boilings if boiling.props['boiling_model'].line.name == LineName.WATER]
-    # if not water_boilings:
-    #     start_from = 1
-    # else:
-    #     start_from = boilings.index(water_boilings[-1]) + 1
-    # todo: hardcode
-    start_from = 10 + 1
+    if cast_t(res[None]['max_non_full_cleaning_time']) >= cast_t('12:00'):
+        for i in tqdm(range(len(boilings) - 1)):
+            boilings = make_boilings(boiling_plan_df)
+            # sort by order in schedule
+            boilings = list(sorted(boilings, key=lambda boiling: listify(schedule['master']['boiling']).index([b for b in listify(schedule['master']['boiling']) if b.props['boiling_id'] == boiling.props['boiling_id']])))
 
-    for i in tqdm(range(len(boilings))):
-        # small optimization. # todo: del
-        # a = cleaning_boilings[1]['pouring']['first']['termizator'].y[0]
-        # b = cleaning_boilings[i]['pouring']['first']['termizator'].y[0]
-        # c = cleaning_boilings[-1]['pouring']['first']['termizator'].x[0]
-        #
-        # if cleaning_boilings[i] and c - b > cast_t('14:00') or b - a > cast_t('14:00')
-        #     logger.info(f'Skipping boiling {i}')
-        #     continue
+            water_boilings = [boiling for boiling in boilings if boiling.line.name == LineName.WATER]
+            if i < boilings.index(water_boilings[-1]):
+                continue
 
-        if i != 0 and i < start_from:
-            # start from water boilings
-            continue
-
-        boilings = make_boilings(boiling_plan_df)
-        cleaning_boilings = [None] + boilings[0:len(boilings) - 1]
-        cleaning_boiling = cleaning_boilings[i]
-        schedule = make_schedule(boilings, cleaning_boiling=cleaning_boiling, start_times=start_times)
-        res[i] = calc_schedule_stats(schedule)
-
-        if not cleaning_boiling and cast_t(res[i]['max_non_full_cleaning_time']) < cast_t('12:00'):
-            # no further search needed
-            break
-
-        # # todo: del, for faster result only - find first one only
-        # if cast_t(res[i]['max_non_full_cleaning_time']) < cast_t('12:00'):
-        #     break
+            schedule = make_schedule(boilings, cleaning_boiling=boilings[i], start_times=start_times)
+            res[i] = calc_schedule_stats(schedule)
 
     suitable = {k: v for k, v in res.items() if cast_t(v['max_non_full_cleaning_time']) < cast_t('12:00')}
 
-    # todo: uncom
-    # assert len(suitable) > 0, 'Не получилось вставить мойку термизатора с учетом правила отсутствия непрерывной работы термизатора на протяжении 12 часов подряд. '
-    # todo: delete
     if suitable:
         res = suitable
         best = min(res.items(), key=lambda v: v[1]['total_time'])
     else:
-        # did not find any suitable - remove no cleaning suitable
         res.pop(0)
-        # res_items = list(sorted(res.items(), key=lambda v: v[1]['max_non_full_cleaning_time']))[:12]
         best = min(res.items(), key=lambda v: v[1]['max_non_full_cleaning_time'])
+
     logger.info(f'Best cleaning_boiling: {best}')
 
     boilings = make_boilings(boiling_plan_df, first_group_id=first_group_id)
-    cleaning_boilings = [None] + boilings[0:len(boilings) - 1]
-    cleaning_boiling = cleaning_boilings[best[0]]
+    cleaning_boiling = None if best is None else boilings[best]
     return make_schedule(boilings, cleaning_boiling=cleaning_boiling, start_times=start_times)
