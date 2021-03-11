8171bd705474fb362ceca512799cca28832c6354
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-03-11 14:50:07 +0300

    cream_cheese_boilings

---
+++ app/schedule_maker/departments/mascarpone/algo/cream_cheese_boilings.py
@@ -0,0 +1,44 @@
+from utils_ak.block_tree import *
+from utils_ak.iteration import *
+from utils_ak.builtin import *
+
+from app.schedule_maker.models import *
+
+
+def make_cream_cheese_boiling(boiling_group_df):
+    boiling_model = boiling_group_df.iloc[0]["sku"].made_from_boilings[0]
+
+    boiling_id = boiling_group_df.iloc[0]["boiling_id"]
+    maker, make = init_block_maker(
+        "boiling", boiling_model=boiling_model, boiling_id=boiling_id
+    )
+
+    bt = boiling_model.boiling_technology
+
+    with make("boiling_process"):
+        make("pouring", size=(bt.cooling_time // 5, 0))
+        make("separation", size=(bt.separation_time // 5, 0))
+        make("salting", size=(bt.salting_time // 5, 0))
+        make("separation", size=(bt.separation_time // 5, 0))
+        make("salting", size=(bt.salting_time // 5, 0))
+        make("P", size=(bt.p_time // 5, 0))
+    with make(
+        "packing_process",
+        x=(maker.root["boiling_process"]["separation"][-1].x[0] - bt.p_time // 5, 0),
+        push_func=add_push,
+    ):
+        make("P", size=(bt.p_time // 5, 0))
+
+        packing_time = sum(
+            [
+                row["kg"] / row["sku"].packing_speed * 60
+                for i, row in boiling_group_df.iterrows()
+            ]
+        )
+        packing_time = int(custom_round(packing_time, 5, "ceil"))
+
+        make(
+            "packing",
+            size=(packing_time // 5, 0),
+        )
+    return maker.root
