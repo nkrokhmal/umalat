642adcbe635057f708c3d71a1dd64b0872d5f13f
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-03-11 11:49:08 +0300

    upd

--- app/schedule_maker/departments/mascarpone/algo/schedule.py
+++ app/schedule_maker/departments/mascarpone/algo/schedule.py
@@ -1,78 +1,15 @@
 from utils_ak.block_tree import *
 
-from app.schedule_maker.departments.ricotta.algo.boilings import *
-from app.schedule_maker.departments.ricotta.algo.cleanings import *
 from app.schedule_maker.models import *
 
 validator = ClassValidator(window=3)
 
 
 def validate(b1, b2):
-    for line_num in range(3):
-        if (
-            line_num not in b1.props["line_nums"]
-            or line_num not in b2.props["line_nums"]
-        ):
-            # skip if no common line number is used
-            continue
-
-        boiling1 = listify(b1["boiling_sequence"]["boiling"])[
-            b1.props["line_nums"].index(line_num)
-        ]
-        boiling2 = listify(b2["boiling_sequence"]["boiling"])[
-            b2.props["line_nums"].index(line_num)
-        ]
-
-        validate_disjoint_by_axis(boiling1, boiling2)
-
-    validate_disjoint_by_axis(b1["analysis_group"], b2["analysis_group"])
-    validate_disjoint_by_axis(b1["packing"], b2["packing"])
-
-    # five minute pause between bigger to smaller packing size
-    sku1 = b1.props["skus"][-1]
-    sku2 = b2.props["skus"][0]
-
-    # todo: del
-    if sku1.weight_netto and sku2.weight_netto:
-        if sku1.weight_netto > sku2.weight_netto:
-            assert b1["packing"].y[0] + 1 <= b2["packing"].x[0]
+    pass
 
 
 validator.add("boiling_group", "boiling_group", validate)
-
-
-def validate(b1, b2):
-    for line_num in range(3):
-        bath_cleaning = listify(b2["bath_cleaning"])[line_num]
-        if line_num in b1.props["line_nums"]:
-            boiling = listify(b1["boiling_sequence"].children)[
-                b1.props["line_nums"].index(line_num)
-            ]
-            validate_disjoint_by_axis(boiling, bath_cleaning)
-
-            assert boiling.y[0] + 1 <= bath_cleaning.x[0]
-
-
-validator.add("boiling_group", "bath_cleanings", validate)
-
-
-def validate(b1, b2):
-    validate_disjoint_by_axis(
-        b1["analysis_group"]["preparation"], b2["container_cleaning_1"]
-    )
-    # add extra 5 minutes
-    assert (
-        b1["analysis_group"]["preparation"].y[0] + 1 <= b2["container_cleaning_1"].x[0]
-    )
-    validate_disjoint_by_axis(
-        b1["analysis_group"]["analysis"], b2["container_cleaning_2"]
-    )
-    validate_disjoint_by_axis(
-        b1["analysis_group"]["pumping"], b2["container_cleaning_3"]
-    )
-
-
-validator.add("boiling_group", "container_cleanings", validate)
 
 
 def _equal_prefixes(lst1, lst2):
---
+++ app/schedule_maker/departments/mascarpone/frontend/frontend_test.py
@@ -0,0 +1,37 @@
+import os
+
+os.environ["environment"] = "interactive"
+
+from app.schedule_maker import *
+
+
+def test_make_frontend_boiling():
+    boiling_model = cast_model(RicottaBoiling, 9)
+    print(make_frontend_boiling(make_boiling(boiling_model)))
+
+
+def test_make_frontend():
+    from utils_ak.loguru import configure_loguru_stdout
+
+    configure_loguru_stdout("INFO")
+    boiling_plan_df = generate_random_boiling_plan()
+    schedule = make_schedule(boiling_plan_df)
+    frontend = make_frontend(schedule)
+    print(frontend)
+
+
+def test_drawing():
+    from utils_ak.loguru import configure_loguru_stdout
+
+    configure_loguru_stdout("INFO")
+    boiling_plan_df = generate_random_boiling_plan()
+    schedule = make_schedule(boiling_plan_df)
+    frontend = make_frontend(schedule)
+    draw_excel_frontend(frontend, RICOTTA_STYLE, open_file=True)
+
+
+if __name__ == "__main__":
+    test_make_frontend_boiling()
+    test_make_frontend()
+
+    test_drawing()
