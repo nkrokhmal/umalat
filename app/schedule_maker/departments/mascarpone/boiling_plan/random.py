83c80d07cc688d881fc746c24621b69b0c12c0a6
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-03-14 22:28:39 +0300

    mascarpone: boiling_plan maker

--- app/schedule_maker/departments/mascarpone/boiling_plan/random.py
+++ app/schedule_maker/departments/mascarpone/boiling_plan/random.py
@@ -2,11 +2,11 @@ import random
 from app.schedule_maker.models import *
 
 
-def generate_random_boiling_plan_mascarpone(n=3, seed=12):
+def _generate_random_boiling_group(sku_model, boiling_model, n=3, seed=12):
     random.seed(seed)
 
-    skus = fetch_all(MascarponeSKU)
-    models = fetch_all(MascarponeBoiling)
+    skus = fetch_all(sku_model)
+    models = fetch_all(boiling_model)
 
     values = []
     for i in range(n):
@@ -24,23 +24,17 @@ def generate_random_boiling_plan_mascarpone(n=3, seed=12):
     return df
 
 
-def generate_random_boiling_plan_cream_cheese(n=3, seed=12):
-    random.seed(seed)
+def generate_random_boiling_plan(n_groups=4, seed=17):
+    dfs = []
+    for i in range(n_groups):
+        group_type = random.choice(["Mascarpone", "CreamCheese"])
+        sku_model = globals()[group_type + "SKU"]
+        boiling_model = globals()[group_type + "Boiling"]
+        df = _generate_random_boiling_group(sku_model, boiling_model, seed=seed + i)
+        dfs.append(df)
 
-    skus = fetch_all(CreamCheeseSKU)
-    models = fetch_all(CreamCheeseBoiling)
+    res = pd.concat(dfs, axis=0)
 
-    values = []
-    for i in range(n):
-        boiling_model = random.choice(models)
-
-        for j in range(2):
-            sku = random.choice(
-                [sku for sku in skus if boiling_model in sku.made_from_boilings]
-            )
-            kg = sku.packing_speed * np.random.uniform(0.6, 0.8) / 2
-            kg = custom_round(kg, 10, "ceil")
-            values.append([i * 2 + j, sku, kg])
-
-    df = pd.DataFrame(values, columns=["boiling_id", "sku", "kg"])
-    return df
+    res["boiling_id"] = range(len(res))
+    res["boiling_id"] //= 2
+    return res
--- app/schedule_maker/departments/mascarpone/boiling_plan/random_test.py
+++ app/schedule_maker/departments/mascarpone/boiling_plan/random_test.py
@@ -4,10 +4,14 @@ os.environ["environment"] = "interactive"
 
 from app.schedule_maker import *
 from app.schedule_maker.departments.mascarpone.boiling_plan.random import *
+from app.schedule_maker.departments.mascarpone.boiling_plan.random import (
+    _generate_random_boiling_group,
+)
 
 
 def test():
-    print(generate_random_boiling_plan_mascarpone())
+    # print(_generate_random_boiling_group(MascarponeSKU, MascarponeBoiling))
+    print(generate_random_boiling_plan())
 
 
 if __name__ == "__main__":
