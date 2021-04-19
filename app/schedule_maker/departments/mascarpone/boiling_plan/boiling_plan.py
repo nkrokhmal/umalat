72f942bc1735a9fef3a72214e49cfe9e37b80a7e
akadaner <arseniikadaner@gmail.com>
2021-04-19 19:40:21 +0300

    update

--- app/schedule_maker/departments/mascarpone/boiling_plan/boiling_plan.py
+++ app/schedule_maker/departments/mascarpone/boiling_plan/boiling_plan.py
@@ -86,7 +86,7 @@ def read_boiling_plan(wb_obj, as_boilings=True):
 
         sourdoughs = []
         for s in sourdough_range.split("-"):
-            if s == "None" or np.isnan(s):
+            if s == "None" or is_none(s):
                 assert (
                     grp.iloc[0]["type"] == "mascarpone" and grp.iloc[0]["is_cream"]
                 ), "Для одной из варок не указаны заквасочники."
