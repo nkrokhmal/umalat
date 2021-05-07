0ceba6c95fb5e760b82b4408999b8d590c6dd02a
nkrokhmal <krokhmal11@mail.ru>
2021-05-07 14:19:28 +0300

    Fix

--- server_build.sh
+++ server_build.sh
@@ -1,1 +1,1 @@
-git pull && sudo docker rm umalat --force && sudo docker build -t nkrokhmal/umalat:dev -f Dockerfile.prod . && sudo docker run --name umalat -d -p 5000:5000 nkrokhmal/umalat:dev
+git pull && sudo docker rm umalat --force && sudo docker build -t nkrokhmal/umalat:dev -f Dockerfile.prod . && sudo docker run --name umalat -d -p 5000:5000 nkrokhmal/umalat:dev2
