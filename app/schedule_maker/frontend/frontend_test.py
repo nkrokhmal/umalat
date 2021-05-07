f3f2de774dfd67bc809455bf359f6ee6d371a022
akadaner <arseniikadaner@gmail.com>
2021-05-07 19:34:26 +0300

    updated params

--- app/schedule_maker/frontend/frontend_test.py
+++ app/schedule_maker/frontend/frontend_test.py
@@ -13,12 +13,15 @@ def test():
     makedirs("schedules/")
     # fn = os.path.join(basedir, "app/data/inputs/2021-02-08 План по варкам.xlsx")
     # fn = os.path.join(basedir, "app/data/inputs/2021-02-17 План по варкам.xlsx")
-    fn = os.path.join(basedir, "app/data/inputs/2021-04-13 План по варкам.xlsx")
+    # fn = os.path.join(basedir, "app/data/inputs/2021-04-13 План по варкам.xlsx")
     fn = os.path.join(basedir, "app/data/inputs/2021-05-07 План по варкам.xlsx")
     boiling_plan_df = read_boiling_plan(fn)
-    start_times = {LineName.WATER: "02:00", LineName.SALT: "10:00"}
-    boilings = make_boilings(boiling_plan_df)
-    schedule = make_schedule(boilings, start_times=start_times)
+    start_times = {LineName.WATER: "02:00", LineName.SALT: "06:00"}
+    # boilings = make_boilings(boiling_plan_df, first_group_id=74)
+    # schedule = make_schedule(boilings, start_times=start_times)
+    schedule = make_schedule_with_boiling_inside_a_day(
+        boiling_plan_df, start_times=start_times, first_group_id=74
+    )
     try:
         frontend = make_frontend(schedule)
     except Exception as e:
