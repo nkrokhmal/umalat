e1f3daa7e41cf86fdf3c6a9ba0cf16fbd5a87a69
akadaner <arseniikadaner@gmail.com>
2021-05-07 19:39:27 +0300

    hotfix

--- init_db.sh
+++ init_db.sh
@@ -1,1 +1,1 @@
-rm -rf migrations && python manage.py db init && python manage.py db migrate && python manage.py db upgrade && python fill_db.py
+rm data.sqlite && rm -rf migrations && python3 manage.py db init && python3 manage.py db migrate && python3 manage.py db upgrade && python3 fill_db.py
