39be7200c982fbbf77ef20affaa4510266715ae5
nkrokhmal <krokhmal11@mail.ru>
2021-05-19 17:52:10 +0300

    Fix

--- tests/functional/mozzarella/test_boiling_plan.py
+++ tests/functional/mozzarella/test_boiling_plan.py
@@ -3,21 +3,21 @@ from tests.conftest import client
 from flask import url_for
 
 
-# def test_get_boiling_plan(client):
-#     with client.test_client() as client:
-#         url = url_for("main.boiling_plan", _external=False)
-#         response = client.get(url)
-#         assert response.status_code == 200
-#
-#
-# def test_post_boiling_plan(client):
-#     filepath = client.config["TEST_MOZZARELLA"]
-#     with client.test_client() as client:
-#         url = url_for("main.boiling_plan", _external=False)
-#         data = {"date": "2021-01-01", "submit": "submit"}
-#         with open(filepath, 'rb') as f:
-#             data['input_file'] = (io.BytesIO(f.read()), "mozzarella.xlsx")
-#         response = client.post(
-#             url, data=data, follow_redirects=True, content_type='multipart/form-data'
-#         )
-#         assert response.status_code == 200
+def test_get_boiling_plan(client):
+    with client.test_client() as client:
+        url = url_for("main.boiling_plan", _external=False)
+        response = client.get(url)
+        assert response.status_code == 200
+
+
+def test_post_boiling_plan(client):
+    filepath = client.config["TEST_MOZZARELLA"]
+    with client.test_client() as client:
+        url = url_for("main.boiling_plan", _external=False)
+        data = {"date": "2021-01-01", "submit": "submit"}
+        with open(filepath, 'rb') as f:
+            data['input_file'] = (io.BytesIO(f.read()), "mozzarella.xlsx")
+        response = client.post(
+            url, data=data, follow_redirects=True, content_type='multipart/form-data'
+        )
+        assert response.status_code == 200
