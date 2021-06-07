cec50b0d7363d816f388d84f9770e9e58bf52d46
akadaner <arseniikadaner@gmail.com>
2021-06-07 14:21:53 +0300

    hotfixes

--- app/scheduler/butter/algo/schedule.py
+++ app/scheduler/butter/algo/schedule.py
@@ -10,6 +10,7 @@ def make_schedule(boiling_plan_df):
     m = BlockMaker("schedule")
     boiling_plan_df = boiling_plan_df.copy()
 
+    # todo soon: del
     boiling_plan_df = boiling_plan_df[boiling_plan_df['boiling'] != cast_model(ButterBoiling, 8)]
 
     sample_row = boiling_plan_df.iloc[0]
@@ -19,6 +20,7 @@ def make_schedule(boiling_plan_df):
     for boiling_id, grp in boiling_plan_df.groupby("boiling_id"):
         m.row(make_boiling(grp))
     m.row('displacement', size=line.displacement_time // 5)
+
     # todo soon: take from line model
     m.row("cleaning", size=20)
     return m.root
