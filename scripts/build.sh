c13a737771af79d7789756d40970b974c198c2af
nkrokhmal <krokhmal11@mail.ru>
2021-06-08 16:45:21 +0300

    Fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-cd .. && cd scripts && sh save_last_batches.sh && cd .. && git checkout db/prod/  && git pull && docker-compose down &&  docker-compose up --build -d && cd scripts && sleep 7 && sh upload_last_batches.sh
+cd .. && cd scripts && sh save_last_batches.sh && cd .. && git checkout db/prod/ && git checkout db/test/  && git pull && docker-compose down &&  docker-compose up --build -d && cd scripts && sleep 7 && sh upload_last_batches.sh
