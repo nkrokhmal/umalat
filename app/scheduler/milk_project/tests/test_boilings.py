df507d8b67f2d7222b880c238dcb0078274a5331
akadaner <arseniikadaner@gmail.com>
2021-06-09 18:27:04 +0300

    milk project boilings

--- app/scheduler/milk_project/tests/test_boilings.py
+++ app/scheduler/milk_project/tests/test_boilings.py
@@ -3,17 +3,27 @@ import os
 os.environ["environment"] = "interactive"
 from app.models import *
 
-from app.scheduler.butter.algo.boilings import *
-from app.scheduler.butter.boiling_plan import *
+from app.scheduler.milk_project.algo.boilings import *
+from app.scheduler.milk_project.boiling_plan import *
 
 
 def test_boiling():
     utils.lazy_tester.configure_function_path()
     boiling_plan_df = generate_random_boiling_plan()
-    boiling_group_df = boiling_plan_df[boiling_plan_df["boiling_id"] == 0]
+    boiling_group_df = boiling_plan_df[boiling_plan_df["boiling_id"] == 2]
     utils.lazy_tester.log(make_boiling(boiling_group_df))
+    utils.lazy_tester.assert_logs()
+
+
+def test_boiling_sequence():
+    utils.lazy_tester.configure_function_path()
+    boiling_plan_df = generate_random_boiling_plan()
+    boiling_group_df = boiling_plan_df[boiling_plan_df["boiling_id"] == 2]
+    boilings = [make_boiling(boiling_group_df) for _ in range(3)]
+    utils.lazy_tester.log(make_boiling_sequence(boilings))
     utils.lazy_tester.assert_logs()
 
 
 if __name__ == "__main__":
     test_boiling()
+    test_boiling_sequence()
