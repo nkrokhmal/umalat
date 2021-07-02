4d3d13e68b6cdb2e10f2868e5525794912a7b9c2
akadaner <arseniikadaner@gmail.com>
2021-07-02 11:07:00 +0300

    milk_project boiling plans

--- app/scheduler/milk_project/tests/test_frontend.py
+++ app/scheduler/milk_project/tests/test_frontend.py
@@ -5,13 +5,17 @@ from app.scheduler.milk_project.boiling_plan import *
 from app.scheduler.milk_project.frontend import *
 
 
-def test_drawing_milk_project(open_file=False):
+def test_drawing_milk_project(boiling_plan_obj=None, open_file=False):
     utils.configure_loguru_stdout("INFO")
     utils.lazy_tester.configure_function_path()
 
-    utils.lazy_tester.configure(local_path="random")
-
-    boiling_plan_df = generate_random_boiling_plan()
+    if not boiling_plan_obj:
+        boiling_plan_df = generate_random_boiling_plan()
+        utils.lazy_tester.configure(local_path="random")
+    else:
+        fn = boiling_plan_obj
+        boiling_plan_df = read_boiling_plan(fn)
+        utils.lazy_tester.configure(local_path=os.path.basename(fn))
 
     schedule = make_schedule(boiling_plan_df)
     utils.lazy_tester.log(schedule)
@@ -20,8 +24,18 @@ def test_drawing_milk_project(open_file=False):
     utils.lazy_tester.log(frontend)
     draw_excel_frontend(frontend, STYLE, open_file=open_file)
 
-    # utils.lazy_tester.assert_logs()
+    utils.lazy_tester.assert_logs()
+
+
+def test_samples():
+    fns = glob.glob(
+        DebugConfig.abs_path("app/data/static/samples/inputs/milk_project/*.xlsx")
+    )
+    fns = [fn for fn in fns if "$" not in fn]
+    for fn in fns:
+        test_drawing_milk_project(fn, open_file=True)
 
 
 if __name__ == "__main__":
-    test_drawing_milk_project(open_file=True)
+    # test_drawing_milk_project(open_file=True)
+    test_samples()
