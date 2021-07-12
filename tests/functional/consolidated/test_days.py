438a8cb21b6c609f86b18f9b86437f06ef92719b
akadaner <arseniikadaner@gmail.com>
2021-07-12 17:40:14 +0300

    test_batch in test_days saved results from sample 1 and sample 2

--- tests/functional/consolidated/test_days.py
+++ tests/functional/consolidated/test_days.py
@@ -10,42 +10,60 @@ def test_day(
     run_mozzarella(
         os.path.join(input_path, prefix + " " + "План по варкам моцарелла.xlsx"),
         prefix=prefix,
+        path=output_path,
         **input_params.get("mozzarella", {})
     )
     run_ricotta(
         os.path.join(input_path, prefix + " " + "План по варкам рикотта.xlsx"),
         prefix=prefix,
+        path=output_path,
         **input_params.get("ricotta", {})
     )
     run_mascarpone(
         os.path.join(input_path, prefix + " " + "План по варкам маскарпоне.xlsx"),
         prefix=prefix,
+        path=output_path,
         **input_params.get("mascarpone", {})
     )
     run_butter(
         os.path.join(input_path, prefix + " " + "План по варкам масло.xlsx"),
         prefix=prefix,
+        path=output_path,
         **input_params.get("butter", {})
     )
     run_milk_project(
         os.path.join(input_path, prefix + " " + "План по варкам милкпроджект.xlsx"),
         prefix=prefix,
+        path=output_path,
         **input_params.get("milk_project", {})
     )
     run_contour_cleanings(
-        output_path, prefix=prefix, **input_params.get("contour_cleanings", {})
+        output_path,
+        output_path=output_path,
+        prefix=prefix,
+        **input_params.get("contour_cleanings", {})
     )
     run_consolidated(
         output_path,
+        output_path=output_path,
         prefix=prefix,
         open_file=open_file,
     )
 
 
+def test_batch(same_output_path=False):
+    paths = glob.glob(DebugConfig.abs_path("app/data/static/samples/inputs/by_day/*"))
+
+    for path in tqdm.tqdm(paths):
+        prefix = os.path.basename(path)
+        output_path = "outputs/" if not same_output_path else path
+        test_day(
+            input_path=path,
+            output_path=output_path,
+            prefix=prefix,
+            open_file=False,
+        )
+
+
 if __name__ == "__main__":
-    test_day(
-        input_path="/Users/arsenijkadaner/Yandex.Disk.localized/master/code/git/2020.10-umalat/umalat/app/data/static/samples/inputs/by_day/sample1",
-        output_path="/Users/arsenijkadaner/Yandex.Disk.localized/master/code/git/2020.10-umalat/umalat/app/data/static/samples/inputs/by_day/sample1",
-        prefix="sample1",
-        open_file=False,
-    )
+    test_batch(True)
