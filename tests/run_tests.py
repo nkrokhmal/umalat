f3cccde14e01fee7c541dd85fb01d9eedf70f2b5
nkrokhmal <krokhmal11@mail.ru>
2021-07-16 11:48:17 +0300

    Fix

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,8 +4,11 @@ import subprocess
 def get_last_commit_name():
     process = subprocess.Popen(['git', 'log', 'master', '--format=%B', '-1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     last_commit_name = str(process.stdout.read()).lower()
+
     print(last_commit_name)
+
     if "--no-tests" in last_commit_name or "--no-test" in last_commit_name:
+        # todo: fast tests
         pass
     else:
         from tests.runners.runner import run_test
