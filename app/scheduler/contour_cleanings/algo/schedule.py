23edc27c9fe59359a2e5759bb2e7552555dfaa5a
akadaner <arseniikadaner@gmail.com>
2021-07-17 09:16:05 +0300

    hotfixes

--- app/scheduler/contour_cleanings/algo/schedule.py
+++ app/scheduler/contour_cleanings/algo/schedule.py
@@ -80,22 +80,22 @@ def _make_contour_1(schedules, order=(0, 1, 2), milk_project_end_time=None, adyg
                 assert len(_boilings) <= 9, f'Слишком много наливов на смеси {percent}'
 
             if len(_boilings) >= 10:
-                values.append([percent, _boilings[8]['pouring']['first']['termizator'].y[0]])
+                values.append([percent, _boilings[8]['pouring']['first']['termizator'].y[0], '01:50'])
 
             if len(_boilings) > 0:
-                values.append([percent, _boilings[-1]['pouring']['first']['termizator'].y[0]])
+                values.append([percent, _boilings[-1]['pouring']['first']['termizator'].y[0], '01:50'])
             else:
                 # no boilings found today
-                values.append([percent, cast_t('10:00')])
+                values.append([percent, cast_t('10:00'), '01:05'])
 
-        df = pd.DataFrame(values, columns=['percent', 'pouring_end'])
-        df = df[['percent', 'pouring_end']]
+        df = pd.DataFrame(values, columns=['percent', 'pouring_end', 'time'])
+        df = df[['percent', 'pouring_end', 'time']]
         df = df.sort_values(by='pouring_end')
         values = df.values.tolist()
 
-        for percent, end in values:
+        for percent, end, time in values:
             m.row('cleaning', push_func=AxisPusher(start_from=end, validator=CleaningValidator(ordered=False)),
-                  size=cast_t('01:50'),
+                  size=cast_t(time),
                   label=f'Танк смесей {percent}')
 
     m.row('cleaning', push_func=AxisPusher(validator=CleaningValidator()),
