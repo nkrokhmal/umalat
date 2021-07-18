26635fe00df672baaa3cae6eeb55c67043a51f43
akadaner <arseniikadaner@gmail.com>
2021-07-18 10:18:22 +0300

    hotfix

--- app/scheduler/contour_cleanings/tests/test_calc_scotta.py
+++ app/scheduler/contour_cleanings/tests/test_calc_scotta.py
@@ -12,8 +12,8 @@ def _test(path, prefix):
     print(calc_scotta_input_tanks(schedules))
 
 
-if __name__ == "__main__":
-    _test(
-        "/Users/marklidenberg/Yandex.Disk.localized/Загрузки/umalat/2021-07-17/approved",
-        "2021-07-17",
-    )
+# if __name__ == "__main__":
+#     _test(
+#         "/Users/marklidenberg/Yandex.Disk.localized/Загрузки/umalat/2021-07-17/approved",
+#         "2021-07-17",
+#     )
