98b9a949aaa8ebed1971d9de2b368c6cf9c433bc
akadaner <arseniikadaner@gmail.com>
2021-08-02 19:24:08 +0300

    hotfix

--- app/scheduler/adygea/algo/schedule.py
+++ app/scheduler/adygea/algo/schedule.py
@@ -21,14 +21,14 @@ class Validator(ClassValidator):
 def make_schedule(boiling_plan_df, first_boiling_id=1, start_time='07:00'):
     m = BlockMaker("schedule")
     boiling_plan_df = boiling_plan_df.copy()
-    boiling_plan_df["boiling_id"] += first_boiling_id - 1
 
     cur_boiler_num = 0
+    cur_boiling_id = boiling_plan_df['boiling_id'].iloc[0] + first_boiling_id - 1
     for i, row in boiling_plan_df.iterrows():
         for _ in range(row['n_baths']):
-            boiling = make_boiling(row['boiling'], boiling_id=row['boiling_id'], boiler_num=cur_boiler_num)
+            boiling = make_boiling(row['boiling'], boiling_id=cur_boiling_id, boiler_num=cur_boiler_num)
             push(m.root, boiling, push_func=AxisPusher(start_from='last_beg'), validator=Validator())
             cur_boiler_num = (cur_boiler_num + 1) % 4
-
+            cur_boiling_id += 1
     m.root.props.update(x=(cast_t(start_time), 0))
     return m.root
