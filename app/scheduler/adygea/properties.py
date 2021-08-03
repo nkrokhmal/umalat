48d31e4b1a8c2c2cc8fc8700026eb2098bba3607
akadaner <arseniikadaner@gmail.com>
2021-08-03 12:56:39 +0300

    adygea properties

--- app/scheduler/adygea/properties.py
+++ app/scheduler/adygea/properties.py
@@ -6,22 +6,11 @@ from app.enum import *
 
 from pydantic import Field
 
-class RicottaProperties(pydantic.BaseModel):
-    n_boilings: int = Field(0, description='Число варок (используется для расчета скотты)')
-    last_pumping_out_time: str = Field('', description='Конец последнего слива')
-    start_of_ninth_from_the_end_time: str = Field('', description='Начало девятой варки с конца')
+class AdygeaProperties(pydantic.BaseModel):
+    end_time: str = Field('', description='Конец работы адыгейского цеха')
 
 
 def parse_schedule(schedule):
-    props = RicottaProperties()
-    ricotta_boilings = list(schedule.iter(cls='boiling'))
-    with code('scotta input tanks'):
-        props.n_boilings = len(ricotta_boilings)
-
-    props.last_pumping_out_time = cast_time(ricotta_boilings[-1]['pumping_out'].y[0])
-    if len(ricotta_boilings) < 9:
-        props.start_of_ninth_from_the_end_time = cast_time(ricotta_boilings[-1].x[0])
-    else:
-        props.start_of_ninth_from_the_end_time = cast_time(ricotta_boilings[-9].x[0])
-
+    props = AdygeaProperties()
+    props.end_time = cast_time(schedule.y[0])
     return props
