1bde5e27546d6c826392a69e62403ede5f9447d3
nkrokhmal <krokhmal11@mail.ru>
2021-08-12 22:43:10 +0300

    --no-tests

--- app/main/workers/send_file.py
+++ app/main/workers/send_file.py
@@ -10,7 +10,7 @@ def send_file(path, date, department):
     document = open(path, 'rb')
     tb.send_message(
         flask.current_app.config["TELEGRAM_CHAT_FILES_ID"],
-        f"Дата: {date} \n Цех: {department} \n" + b"\xE2\x9C\x85".decode('utf-8') + b'\xE2\x9C\x8C'.decode('utf-8')
+        f"Дата: {date}\n Цех: {department}" + b"\xE2\x9C\x85".decode('utf-8')
     )
     tb.send_document(
         flask.current_app.config["TELEGRAM_CHAT_FILES_ID"],
