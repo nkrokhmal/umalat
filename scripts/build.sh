8d3556321a3b6543d538f8005429a2637911caa6
nkrokhmal <krokhmal11@mail.ru>
2021-08-12 21:36:36 +0300

    Fix --no-tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ sh save_batches.sh &&
 cd .. &&
 git checkout db/prod/data.sqlite  &&
 git pull &&
-docker-compose -f docker-compose-test.yaml up &&
+docker-compose -f docker-compose-test.yaml up --build &&
 docker-compose down &&
 docker-compose up --build -d &&
 cd scripts &&
