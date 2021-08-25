fb341d648ec848fa594c36774ddab63d416dc8b5
nkrokhmal <krokhmal11@mail.ru>
2021-08-25 22:33:12 +0300

    Fix

--- app/main/workers/send_file.py
+++ app/main/workers/send_file.py
@@ -3,6 +3,15 @@ import telebot
 from loguru import logger
 
 
+DEPARTMENT_CHAT_ID = {
+    "Моцарелла": flask.current_app.config["TELEGRAM_CHAT_MOZZARELLA_ID"],
+    "Рикотта": flask.current_app.config["TELEGRAM_CHAT_RICOTTA_ID"],
+    "Маскарпоне": flask.current_app.config["TELEGRAM_CHAT_MASCARPONE_ID"],
+    "Масло": flask.current_app.config["TELEGRAM_CHAT_BUTTER_ID"],
+    "Милкпроджект": flask.current_app.config["TELEGRAM_CHAT_MILKPROJECT_ID"],
+}
+
+
 @rq.job
 def send_file(path, date, department):
     tb = telebot.TeleBot(
@@ -17,5 +26,17 @@ def send_file(path, date, department):
         flask.current_app.config["TELEGRAM_CHAT_FILES_ID"],
         document
     )
+    try:
+        document = open(path, 'rb')
+        tb.send_message(
+            DEPARTMENT_CHAT_ID[department],
+            f"Дата: {date} \nЦех: {department}  " + b"\xE2\x9C\x85".decode('utf-8')
+        )
+        tb.send_document(
+            DEPARTMENT_CHAT_ID[department],
+            document
+        )
+    except:
+        pass
 
 
