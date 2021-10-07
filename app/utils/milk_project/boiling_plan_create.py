06be0b8207231b2b70535eb2bd0eda0febdc1321
nkrokhmal <krokhmal11@mail.ru>
2021-10-07 19:31:37 +0300

    Fix

--- app/utils/milk_project/boiling_plan_create.py
+++ app/utils/milk_project/boiling_plan_create.py
@@ -1,30 +1,43 @@
+import pandas as pd
+
 from app.imports.runtime import *
 from app.models import *
 from app.utils.features.merge_boiling_utils import Boilings
 
 
 def boiling_plan_create(df):
-    df["plan"] = df["plan"].apply(lambda x: round(x))
-    df["percent"] = df["sku"].apply(lambda x: x.made_from_boilings[0].percent)
-    df["group"] = df["sku"].apply(lambda x: x.group.name)
-    df["output"] = df["sku"].apply(lambda x: x.made_from_boilings[0].output_kg)
-    df["boiling_type"] = df["sku"].apply(lambda x: x.made_from_boilings[0].name)
+    if not df.empty:
+        df["plan"] = df["plan"].apply(lambda x: round(x))
+        df["percent"] = df["sku"].apply(lambda x: x.made_from_boilings[0].percent)
+        df["group"] = df["sku"].apply(lambda x: x.group.name)
+        df["output"] = df["sku"].apply(lambda x: x.made_from_boilings[0].output_kg)
+        df["boiling_type"] = df["sku"].apply(lambda x: x.made_from_boilings[0].name)
 
-    result, boiling_number = handle_milk_project(df)
-    result["kg"] = result["plan"]
-    result["name"] = result["sku"].apply(lambda sku: sku.name)
-    result = result[
-        [
-            "id",
-            "sku",
-            "group",
-            # "output",
-            "name",
-            "boiling_type",
-            "kg",
+        result, boiling_number = handle_milk_project(df)
+        result["kg"] = result["plan"]
+        result["name"] = result["sku"].apply(lambda sku: sku.name)
+        result = result[
+            [
+                "id",
+                "sku",
+                "group",
+                # "output",
+                "name",
+                "boiling_type",
+                "kg",
+            ]
         ]
-    ]
-    return result
+        return result
+    else:
+        return pd.DataFrame(columns=[
+                "id",
+                "sku",
+                "group",
+                # "output",
+                "name",
+                "boiling_type",
+                "kg",
+            ])
 
 
 def proceed_order(order, df, boilings_milk_project, boilings_count=1):
