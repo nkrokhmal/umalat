a90145c2b182fdd02435d59c799f5e15e8f39fb8
nkrokhmal <krokhmal11@mail.ru>
2021-11-05 19:34:11 +0300

    Revert "Fix"
    This reverts commit a70946bf2d10db445960de6b056e0b709537386f.

--- db/test/migrations/versions/c437ee44d582_.py
+++ db/test/migrations/versions/c437ee44d582_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 1717be1b824b
+Revision ID: c437ee44d582
 Revises: 
-Create Date: 2021-11-02 22:03:51.561201
+Create Date: 2021-10-18 18:08:09.565185
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1717be1b824b'
+revision = 'c437ee44d582'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -354,7 +354,6 @@ def upgrade():
     sa.Column('adding_lactic_acid_time', sa.Integer(), nullable=True),
     sa.Column('output_ton', sa.Integer(), nullable=True),
     sa.Column('pumping_off_time', sa.Integer(), nullable=True),
-    sa.Column('pumping_off_pause_time', sa.Integer(), nullable=True),
     sa.Column('ingredient_time', sa.Integer(), nullable=True),
     sa.Column('line_id', sa.Integer(), nullable=True),
     sa.ForeignKeyConstraint(['id'], ['boiling_technologies.id'], ),
