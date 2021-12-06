1435f1531a9942d3c46604fdff8cef9404a41edb
nkrokhmal <krokhmal11@mail.ru>
2021-12-06 21:44:58 +0300

    --no-tests

--- app/utils/mascarpone/schedule_task.py
+++ app/utils/mascarpone/schedule_task.py
@@ -2,6 +2,7 @@ from app.models import MascarponeSKU
 from app.utils.base.schedule_task import BaseScheduleTask
 from app.utils.files.utils import create_dir
 from app.imports.runtime import *
+from app.utils.features.draw_utils import *
 
 
 class MascarponeScheduleTask(BaseScheduleTask[MascarponeSKU]):
@@ -18,10 +19,10 @@ class MascarponeScheduleTask(BaseScheduleTask[MascarponeSKU]):
 
         df_task = pd.read_csv(path, sep=";")
         df_task.drop(df_task.index, inplace=True)
-
+        self.df["coeff"] = self.df["boiling"].apply(lambda x: x.output_coeff)
         for boiling_group_id, grp in self.df.groupby("batch_id"):
             for i, row in grp.iterrows():
-                kg = round(row["original_kg"])
+                kg = round(row["original_kg"] * row["coeff"])
                 boxes_count = math.ceil(
                     row["original_kg"]
                     / row["sku"].in_box
@@ -38,3 +39,29 @@ class MascarponeScheduleTask(BaseScheduleTask[MascarponeSKU]):
                 ]
                 df_task = df_task.append(dict(zip(columns, values)), ignore_index=True)
         df_task.to_csv(path, index=False, sep=";")
+
+    def draw_task_original(self, excel_client, cur_row, task_name):
+        df_filter = self.df
+        index = 1
+
+        cur_row, excel_client = draw_header(excel_client, self.date, cur_row, task_name)
+
+        self.df["coeff"] = self.df["boiling"].apply(lambda x: x.output_coeff)
+        for sku_name, grp in df_filter.groupby("sku_name"):
+            kg = round((grp["kg"] * grp["coeff"]).sum())
+            boxes_count = math.ceil(
+                grp["kg"].sum()
+                / grp.iloc[0]["sku"].in_box
+                / grp.iloc[0]["sku"].weight_netto
+            )
+            values = [
+                index,
+                sku_name,
+                grp.iloc[0]["sku"].in_box,
+                kg,
+                boxes_count,
+                grp.iloc[0]["sku"].code,
+            ]
+            excel_client, cur_row = draw_schedule_raw(excel_client, cur_row, values)
+            index += 1
+        return cur_row
