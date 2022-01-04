8bd722eddffbef4592d381b2abb545cb053cf2b7
nkrokhmal <krokhmal11@mail.ru>
2022-01-04 22:40:48 +0300

    --no-tests

--- db/prod/migrations/versions/7c276cc4d3e9_.py
+++ db/prod/migrations/versions/7c276cc4d3e9_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 07db7a55eae5
+Revision ID: 7c276cc4d3e9
 Revises: 
-Create Date: 2022-01-03 20:21:28.094826
+Create Date: 2022-01-04 22:39:38.481754
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '07db7a55eae5'
+revision = '7c276cc4d3e9'
 down_revision = None
 branch_labels = None
 depends_on = None
--- db/test/migrations/versions/240e02f26abd_.py
+++ db/test/migrations/versions/240e02f26abd_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 306e5db62585
+Revision ID: 240e02f26abd
 Revises: 
-Create Date: 2022-01-03 20:21:12.040972
+Create Date: 2022-01-04 22:40:19.663287
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '306e5db62585'
+revision = '240e02f26abd'
 down_revision = None
 branch_labels = None
 depends_on = None
