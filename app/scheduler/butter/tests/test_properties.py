9720ce94ef433c8c7f49db8ebfd21864a63c029c
marklidenberg <marklidenberg@gmail.com>
2022-01-21 17:05:26 +0300

    butter load_properties test

--- app/scheduler/butter/tests/test_properties.py
+++ app/scheduler/butter/tests/test_properties.py
@@ -3,14 +3,29 @@ from app.scheduler.ricotta.properties import *
 from pprint import pprint
 
 
-def test_properties(path, prefix):
-    schedules = load_schedules(path, prefix, departments=["ricotta"])
-    props = cast_properties(schedules["ricotta"])
-    pprint(dict(props))
+def test_batch():
+    paths = glob.glob(
+        config.abs_path(
+            "app/data/static/samples/inputs/by_day/*"
+        )
+    )
+    paths = [fn for fn in paths if "$" not in fn]
+    prefixes = [os.path.basename(fn) for fn in paths]
+
+    for i, fn in enumerate(utils.tqdm(paths, desc=lambda v: v)):
+        _test(paths[i], prefixes[i], open_file=False, prefix=str(i))
+
+
+def _test(path, path_prefix, *args, **kwargs):
+    utils.lazy_tester.configure_function_path()
+    warnings.filterwarnings("ignore")
+    utils.lazy_tester.configure(local_path=os.path.basename(path + path_prefix))
+
+    schedules = load_schedules(path, path_prefix, departments=["ricotta"])
+    outputs = cast_properties(schedules["ricotta"])
+    utils.lazy_tester.log(outputs)
+    utils.lazy_tester.assert_logs()
 
 
 if __name__ == "__main__":
-    test_properties(
-        "/Users/marklidenberg/Yandex.Disk.localized/master/code/git/2020.10-umalat/umalat/app/data/static/samples/inputs/by_day/sample1",
-        "sample1",
-    )
+    test_batch()
