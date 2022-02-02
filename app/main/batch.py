05adc5a64a2331980ea38009ceebc09f224c488f
marklidenberg <marklidenberg@gmail.com>
2022-02-02 11:19:39 +0300

    batch test --no-tests

--- app/main/batch.py
+++ app/main/batch.py
@@ -23,7 +23,6 @@ def save_batches():
         return response
 
 
-
 @main.route("/upload_batches", methods=["GET", "POST"])
 def upload_batches():
     batch_path = os.path.join(flask.current_app.config["BATCH_NUMBERS_DIR"], "batches.json")
