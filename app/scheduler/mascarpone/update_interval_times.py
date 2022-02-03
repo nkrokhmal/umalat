b5a8de7cac7e0f5a329f78ba35385196bee28866
marklidenberg <marklidenberg@gmail.com>
2022-02-03 20:10:24 +0300

    temporary workaroung --no-tests

--- app/scheduler/mascarpone/update_interval_times.py
+++ app/scheduler/mascarpone/update_interval_times.py
@@ -8,33 +8,35 @@ def update_interval_times(schedule_wb, boiling_plan_df):
     boiling_plan_df['start'] = ''
     boiling_plan_df['finish'] = ''
 
-    with code('fill cream cheese and cottage cheese'):
-        boilings = [b for b in schedule_info.get('cream_cheese_boilings', []) if b['type'] == 'cream_cheese']
-        boilings = list(sorted(boilings, key=lambda b: b['interval'][0]))
+    try:
+        with code('fill cream cheese and cottage cheese'):
+            boilings = [b for b in schedule_info.get('cream_cheese_boilings', []) if b['type'] == 'cream_cheese']
+            boilings = list(sorted(boilings, key=lambda b: b['interval'][0]))
+            if len(boilings) > 0:
+                grouped = list(boiling_plan_df[boiling_plan_df['type'] == 'cream_cheese'].groupby(['full_type', 'absolute_batch_id']))
+                grouped = sorted(grouped, key=lambda v: v[1].index[0])
+                for i, ((full_type, batch_id), grp) in enumerate(grouped):
+                    boiling_plan_df.loc[grp.index, 'start'] = boilings[i]['interval_time'][0]
+                    boiling_plan_df.loc[grp.index, 'finish'] = boilings[i]['interval_time'][1]
 
-        if len(boilings) > 0:
-            grouped = list(boiling_plan_df[boiling_plan_df['type'] == 'cream_cheese'].groupby(['full_type', 'absolute_batch_id']))
-            grouped = sorted(grouped, key=lambda v: v[1].index[0])
-            for i, ((full_type, batch_id), grp) in enumerate(grouped):
-                boiling_plan_df.loc[grp.index, 'start'] = boilings[i]['interval_time'][0]
-                boiling_plan_df.loc[grp.index, 'finish'] = boilings[i]['interval_time'][1]
+        with code('fill mascarpone and cream'):
+            if len(schedule_info.get('mascarpone_boiling_groups', [])) > 0:
+                for boiling_type in ['cream', 'mascarpone']:
+                    if len(boiling_plan_df['full_type'] == boiling_type) == 0:
+                        continue
+                    interval_times = {}
 
-    with code('fill mascarpone and cream'):
-        if len(schedule_info.get('mascarpone_boiling_groups', [])) > 0:
-            for boiling_type in ['cream', 'mascarpone']:
-                if len(boiling_plan_df['full_type'] == boiling_type) == 0:
-                    continue
-                interval_times = {}
+                    for boiling in [b for b in schedule_info['mascarpone_boiling_groups'] if b['type'] == boiling_type]:
+                        interval_times[boiling['boiling_id']] = boiling['interval_time']
 
-                for boiling in [b for b in schedule_info['mascarpone_boiling_groups'] if b['type'] == boiling_type]:
-                    interval_times[boiling['boiling_id']] = boiling['interval_time']
-
-                boiling_plan_df['start'] = np.where(boiling_plan_df['full_type'] == boiling_type,
-                                                    boiling_plan_df['absolute_batch_id'].apply(
-                                                        lambda batch_id: interval_times.get(batch_id, [None, None])[0]),
-                                                    boiling_plan_df['start'])
-                boiling_plan_df['finish'] = np.where(boiling_plan_df['full_type'] == boiling_type,
-                                                     boiling_plan_df['absolute_batch_id'].apply(
-                                                         lambda batch_id: interval_times.get(batch_id, [None, None])[1]),
-                                                     boiling_plan_df['finish'])
+                    boiling_plan_df['start'] = np.where(boiling_plan_df['full_type'] == boiling_type,
+                                                        boiling_plan_df['absolute_batch_id'].apply(
+                                                            lambda batch_id: interval_times.get(batch_id, [None, None])[0]),
+                                                        boiling_plan_df['start'])
+                    boiling_plan_df['finish'] = np.where(boiling_plan_df['full_type'] == boiling_type,
+                                                         boiling_plan_df['absolute_batch_id'].apply(
+                                                             lambda batch_id: interval_times.get(batch_id, [None, None])[1]),
+                                                         boiling_plan_df['finish'])
+    except:
+        pass
     return boiling_plan_df
