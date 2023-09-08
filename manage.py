d61a110b857d60d78f712e7ebdf47e2f4627cffa
nkrokhmal <krokhmal11@mail.ru>
2023-09-08 12:28:43 +0100

    Fix: manage.py

--- manage.py
+++ manage.py
@@ -10,5 +10,11 @@ app, rq = create_app()
 create_manager(app)
 
 
+@app.cli.command("run_worker")
+def run_worker():
+    default_worker = rq.get_worker()
+    default_worker.work()
+
+
 if __name__ == "__main__":
     app.run()
