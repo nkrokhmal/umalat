9d63b7e5072fda26c4a3fd1dcc0d3bb2f3a46142
marklidenberg <marklidenberg@gmail.com>
2023-10-01 22:14:06 +0400

    feat: disable optimization in case of one sheet

--- app/scheduler/mozzarella/make_schedule/make_schedule.py
+++ app/scheduler/mozzarella/make_schedule/make_schedule.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 from app.enum import LineName
 from app.lessmore.utils.get_repo_path import get_repo_path
 from app.scheduler.boiling_plan_like import BoilingPlanLike
@@ -30,6 +32,14 @@ def make_schedule(
         validate=validate,
         first_batch_ids_by_type=first_batch_ids_by_type,
     )
+
+    # - Disable optimizing water gaps if there is only one sheet. It is the case when we upload schedule-file where order is strict
+
+    if len(boiling_plan_df["sheet"].unique()) == 1:
+        if optimize_water_gaps or optimize_start_configurations:
+            logger.warning("Optimizing is disabled because there is only one sheet in the schedule-file")
+            optimize_water_gaps = False
+            optimize_start_configurations = False
 
     # - Make schedule
 
