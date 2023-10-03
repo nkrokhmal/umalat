f0c0540721577188a30cbddfdf6f962e17133624
root <root@825033-cn72368.tmweb.ru>
2023-10-03 23:08:45 +0300

    Fix: build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@ sh save_batches.sh &&
 cd .. &&
 git checkout db/prod/data.sqlite  &&
 git pull &&
-docker compose -f docker-compose-test.yaml up --build &&
-docker compose down &&
-docker compose up --build -d &&
+docker-compose -f docker-compose-test.yaml up --build &&
+docker-compose down &&
+docker-compose up --build -d &&
 cd scripts &&
 sleep 10 &&
 sh upload_batches.sh
