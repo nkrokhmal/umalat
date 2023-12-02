82d55d4e92f8cd25f1dbb33f0e53e648c7f09785
marklidenberg <marklidenberg@gmail.com>
2023-12-02 13:40:21 +0100

    update

--- app/scheduler/brynza/make_schedule/make_schedule.py
+++ app/scheduler/brynza/make_schedule/make_schedule.py
@@ -57,7 +57,9 @@ def make_packing_schedule(
 
     m.row(
         "packing_brynza",
-        size=round((df1["kg"].sum() / df1["sku"].apply(lambda sku: sku.packing_speed + 175)).sum() * 12),
+        size=round(
+            (df1["kg"].sum() / df1["sku"].apply(lambda sku: sku.packing_speed + 175)).sum() * 12
+        ),  # todo later: parameters
         label=label,
     )
     m.row("small_cleaning", size=5)
@@ -73,7 +75,7 @@ def make_packing_schedule(
         boiling = grp.iloc[0]["boiling"]
         total_kg = grp["kg"].sum()
         packing_speed = (
-            grp["sku"].iloc[0].packing_speed or 0 + 267
+            grp["sku"].iloc[0].packing_speed or 0 + 267  # todo later: parameters
         )  # note: packing_speed is the same for all skus in adygea # todo later: check [@marklidenberg]
 
         # crop to pieces of 200kg
