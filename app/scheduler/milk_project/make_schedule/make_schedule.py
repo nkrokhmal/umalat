7a4d21459d4e6555faa50a7be8a1a21ccdfe8175
marklidenberg <marklidenberg@gmail.com>
2024-05-21 23:59:11 +0600

    update

---
+++ app/scheduler/milk_project/make_schedule/make_schedule.py
@@ -0,0 +1,53 @@
+from utils_ak.block_tree.block_maker import BlockMaker
+from utils_ak.builtin.collection import crop_to_chunks, remove_duplicates
+from utils_ak.iteration.simple_iterator import iter_pairs
+
+from app.lessmore.utils.get_repo_path import get_repo_path
+from app.scheduler.common.time_utils import cast_t
+from app.scheduler.milk_project.make_schedule._boilings import make_boiling, make_boiling_sequence
+from app.scheduler.milk_project.to_boiling_plan import BoilingPlanLike, to_boiling_plan
+
+
+def make_schedule(
+    boiling_plan: BoilingPlanLike,
+    start_time="07:00",
+    first_batch_ids_by_type: dict = {"milk_project": 1},
+) -> dict:
+    # - Process boiling plna
+
+    boiling_plan_df = to_boiling_plan(boiling_plan, first_batch_ids_by_type=first_batch_ids_by_type).copy()
+
+    # - Make schedule
+
+    m = BlockMaker("schedule")
+    boilings_ids = remove_duplicates(boiling_plan_df["boiling_id"])
+
+    chunks = list(crop_to_chunks(boilings_ids, 3))
+    for ids, next_ids in iter_pairs(chunks, method="any_suffix"):
+        _df = boiling_plan_df[boiling_plan_df["boiling_id"].isin(ids)]
+        boilings = []
+        for boiling_id, grp in _df.groupby("boiling_id"):
+            boilings.append(make_boiling(grp))
+        boiling_sequence = make_boiling_sequence(boilings)
+        m.push_row(boiling_sequence)
+
+        if next_ids:
+            m.push_row("pouring_off", size=1)
+    m.root.props.update(x=(cast_t(start_time), 0))
+
+    return {"schedule": m.root, "boiling_plan_df": boiling_plan_df}
+
+
+def test():
+    print(
+        make_schedule(
+            str(
+                get_repo_path()
+                / "app/data/static/samples/by_department/milk_project/План по варкам милкпроджект 3.xlsx"
+            )
+        )
+    )
+
+
+if __name__ == "__main__":
+    test()
---
+++ app/scheduler/milk_project/wrap_frontend.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+
+from utils_ak.block_tree.block_maker import BlockMaker
+from utils_ak.block_tree.pushers.pushers import add_push
+from utils_ak.numeric.numeric import custom_round
+
+from app.lessmore.utils.get_repo_path import get_repo_path
+from app.scheduler.common.time_utils import cast_time
+from app.scheduler.common.wrap_header import wrap_header
+from app.scheduler.milk_project.make_schedule.make_schedule import make_schedule
+from app.scheduler.milk_project.to_boiling_plan import BoilingPlanLike
+
+
+def wrap_line(schedule):
+    m = BlockMaker(
+        "line",
+        default_row_width=2,
+        default_col_width=2,
+        # props
+        font_size=9,
+    )
+    for child in schedule.children:
+        if child.props["cls"] == "boiling_sequence":
+            for i, block in enumerate(child.children):
+                if i <= 1:
+                    # water collecting
+                    _block = m.copy(block, with_props=True)
+                    _block.update_size(size=(block.size[0], 2))
+                    m.push(_block, push_func=add_push)
+                else:
+                    for element in block.children:
+                        _element = m.copy(element, with_props=True)
+                        _element.update_size(size=(element.size[0], 2))
+                        _element.props.update(x=(element.x[0], (i - 2) * 2))
+                        m.push(_element, push_func=add_push)
+        elif child.props["cls"] == "pouring_off":
+            _child = m.copy(child, with_props=True)
+            _child.update_size(size=(child.size[0], 6))
+            m.push(_child, push_func=add_push)
+    return m.root
+
+
+def wrap_frontend(
+    boiling_plan: BoilingPlanLike,
+    date=None,
+    start_time="07:00",
+    first_batch_ids_by_type={"milk_project": 1},
+) -> dict:
+    # - Get schedule and boiling_plan_df
+
+    output = make_schedule(
+        boiling_plan=boiling_plan,
+        start_time=start_time,
+        first_batch_ids_by_type=first_batch_ids_by_type,
+    )
+
+    schedule = output["schedule"]
+
+    # - Get frontend
+
+    date = date or datetime.now()
+
+    m = BlockMaker(
+        "frontend",
+        default_row_width=1,
+        default_col_width=1,
+        # props
+        axis=1,
+    )
+    m.push_row("stub", size=0)  # start with 1
+
+    # calc start time
+    start_t = int(custom_round(schedule.x[0], 12, "floor"))  # round to last hour
+    start_time = cast_time(start_t)
+
+    m.push(wrap_header(date=date, start_time=start_time, header="График работы цеха милкпроджект"))
+
+    with m.push(start_time=start_time, axis=1):
+        m.push(wrap_line(schedule))
+
+    # - Update and return output
+
+    output["frontend"] = m.root
+
+    return output
+
+
+def test():
+    print(
+        wrap_frontend(
+            str(
+                get_repo_path()
+                / "app/data/static/samples/by_department/milk_project/План по варкам милкпроджект 3.xlsx"
+            )
+        )
+    )
+
+
+if __name__ == "__main__":
+    test()
