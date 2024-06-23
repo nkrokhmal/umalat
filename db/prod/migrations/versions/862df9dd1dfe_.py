c7de6fd66c2f0d94504290710c4539c98bd1e0a8
nkrokhmal <krokhmal11@mail.ru>
2024-06-23 10:15:02 +0300

    Db update

--- db/prod/migrations/versions/862df9dd1dfe_.py
+++ db/prod/migrations/versions/862df9dd1dfe_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: a1dd905c828e
+Revision ID: 862df9dd1dfe
 Revises: 
-Create Date: 2024-06-16 22:22:20.166324
+Create Date: 2024-06-23 10:14:05.942699
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a1dd905c828e'
+revision = '862df9dd1dfe'
 down_revision = None
 branch_labels = None
 depends_on = None
